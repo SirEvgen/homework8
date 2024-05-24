e0cb43958f72401c831ea4872aeedf58ea4063b9
SirEvgen <sandrofa6666666@gmail.com>
2024-05-25 00:43:41 +0300

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,11 @@
+my_set = {2, 3, 8, 6, 3, 7, 8, 6, 7}
+print(my_set)
+my_dict = [6, 7, 1, 8, 3, 2, 1, 6, 7]
+a = 88
+b = 34
+c = a * b
+var = tuple([2, 5, 7, ((c-b)/a)])
+my_set.add(var)
+my_set.add(c)
+my_set.remove(7)
+print(my_set)
